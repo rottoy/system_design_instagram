ea5607cc0630aa40d55bfa5798e1b2e9f1bb41dc
rottoy <wnsgur9609@naver.com>
2022-01-17 00:59:32 +0900

    change dependency in phase dev : mysql to h2; add ddl, dml

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`user_name`,`email`,`password`,`date_of_birth`,`is_public`) VALUES ('teemo','teemo@gmail.com','1234','2000-01-02 12:00:00','Y');
+INSERT INTO `user` (`user_name`,`email`,`password`,`date_of_birth`,`is_public`) VALUES ('darius','darius@gmail.com','1234','2001-02-03 12:00:00','N');
+INSERT INTO `user` (`user_name`,`email`,`password`,`date_of_birth`,`is_public`) VALUES ('garen','garen@gmail.com','1234','2002-03-04 12:00:00','Y');
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE `user` IF EXISTS;
+CREATE TABLE `user`
+(
+    `id`                  int         NOT NULL AUTO_INCREMENT,
+    `user_name`           varchar(45) NOT NULL,
+    `email`               varchar(45) NOT NULL,
+    `password`            varchar(45) NOT NULL,
+    `date_of_birth`       datetime    NOT NULL,
+    `post_update_date`    timestamp   NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `is_public`           char(1)     NOT NULL,
+    `number_of_post`      int         NOT NULL DEFAULT '0',
+    `number_of_follower`  int         NOT NULL DEFAULT '0',
+    `number_of_following` int         NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
